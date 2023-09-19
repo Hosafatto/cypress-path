6ab6d9968f76e5060ef7bc49eef61c84ac009f99
jose-salazar-bc <jose.salazar@bigcommerce.com>
2023-09-18 21:02:06 -0600

    First Validations

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,5 +1,23 @@
 /// <reference types="cypress"/> 
 
-it('Should navigate to the TodoMVC App', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh')
-})
+describe('Introduction to Cypress', () => 
+{
+    it('Should navigate to the TodoMVC App', () => {
+        cy.visit('http://todomvc-app-for-testing.surge.sh')
+        cy.get('.new-todo').type("Clean room{enter}")
+        cy.get('.toggle').click()
+        cy.contains('Clear').click()
+    })
+
+    it('Should do some validations', () => {
+        cy.visit('http://todomvc-app-for-testing.surge.sh')
+        cy.get('.new-todo').type("Clean room{enter}")
+        cy.get('label').should('have.text', 'Clean room')
+        cy.get('.toggle').should('not.be.checked')
+        cy.get('.toggle').click()
+        cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+        cy.contains('Clear').click()
+        cy.get('.todo-list').should('not.have.descendants', 'li')
+    })
+}
+)
